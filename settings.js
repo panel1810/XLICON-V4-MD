e1fb2a0273866a27ba188111dec5b2c83fcdec96
Aliceliese77 <161917506+panel1810@users.noreply.github.com>
2025-01-10 22:05:13 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,11 +41,11 @@ const chalk = require('chalk');
 
 
 //~~~~~~~~< Owner Information>~~~~~~~~~~~~~~\\
-global.ytname = process.env.YT_NAME || "YT: S4 SALMAN YT";
+global.ytname = process.env.YT_NAME || "YT: -";
 
-global.socialm = process.env.GITHUB_USERNAME || "GitHub: salmanytofficial";
+global.socialm = process.env.GITHUB_USERNAME || "GitHub: -";
 
-global.location = process.env.LOCATION || "Pakistan, Lahore, Sabzazar";
+global.location = process.env.LOCATION || "Indonesia";
 
 
 //~~~~~~~~<Session and Bot Details>~~~~~~~~~~~~~\\
@@ -53,15 +53,15 @@ global.SESSION_ID = process.env.SESSION_ID || '';
 
 global.botname = process.env.BOT_NAME || 'XLICON-V4-MD';
 
-global.ownernumber = [process.env.OWNER_NUMBER || '923184070915'];
+global.ownernumber = [process.env.OWNER_NUMBER || '6285763590930'];
 
-global.ownername = process.env.OWNER_NAME || '𝙎𝙖𝙡𝙢𝙖𝙣 𝘼𝙝𝙢𝙖𝙙';
+global.ownername = process.env.OWNER_NAME || 'STEVEN';
 
 
 //~~~~~~<Website and Social Links>~~~~~~~~~~\\
-global.websitex = process.env.WEBSITE_URL || "https://youtube.com/@s4salmanyt";
+global.websitex = process.env.WEBSITE_URL || "https://youtube.com/";
 
-global.wagc = process.env.WHATSAPP_CHANNEL || "https://whatsapp.com/channel/0029VaINgEZK0IBaK1nDtE3f";
+global.wagc = process.env.WHATSAPP_CHANNEL || "https://whatsapp.com/channel/";
 
 
 //~~~~~~~~<Theme and Miscellaneous>~~~~~~~~~~\\
@@ -69,19 +69,19 @@ global.themeemoji = process.env.THEME_EMOJI || '⛩';
 
 global.wm = process.env.WATERMARK || "Xlicon Bot Inc.";
 
-global.botscript = process.env.SCRIPT_LINK || 'https://github.com/salmanytofficial/XLICON-V4-MD';
+global.botscript = process.env.SCRIPT_LINK || 'https://github.com/salmanytofficial/';
 
-global.packname = process.env.PACK_NAME || "𝘼𝙃𝙃𝙈𝙄 𝙆𝙐𝙉";
+global.packname = process.env.PACK_NAME || "XLICON";
 
-global.author = process.env.AUTHOR_NAME || "MΛDΣ BY 𝙎𝘼𝙇𝙈𝘼𝙉 𝘼𝙃𝙈𝘼𝘿";
+global.author = process.env.AUTHOR_NAME || "MΛDΣ BY STEVEN";
 
-global.creator = process.env.CREATOR_NUMBER || "923184070915@s.whatsapp.net";
+global.creator = process.env.CREATOR_NUMBER || "6285763590930@s.whatsapp.net";
 
 
 //~~~~~~~~~~~~~<Bot Settings>~~~~~~~~~~~~~~~\\
 global.xprefix = process.env.PREFIX || '.';
 
-global.premium = [process.env.PREMIUM_NUMBER || '923184070915'];
+global.premium = [process.env.PREMIUM_NUMBER || '6285763590930'];
 
 global.typemenu = process.env.MENU_TYPE || 'v2';
 
@@ -89,7 +89,7 @@ global.typereply = process.env.REPLY_TYPE || 'v4';
 
 global.autoblocknumber = process.env.AUTOBLOCK_COUNTRYCODE || '212';
 
-global.antiforeignnumber = process.env.ANTIFOREIGN_COUNTRYCODE || '91';
+global.antiforeignnumber = process.env.ANTIFOREIGN_COUNTRYCODE || '62';
 
 global.antidelete = process.env.ANTI_DELETE === 'true';
 
